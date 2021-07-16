501873149ff0c9510f9e24db22c540b6c4ed1069
chaitalizn <phchaitali3@gmail.com>
2021-07-16 13:19:41 -0400

    about section style

--- src/component/About/index.js
+++ src/component/About/index.js
@@ -3,16 +3,20 @@ import profileImage from '../../assets/chaitali-patel-photo.png';
 
 function About() {
     return (
-        <section>
-            <img src={profileImage} alt="chaitali patel photo" />
-            <h1 id="about"> Hi, 
-            <br /> <span className="highlightName">I'M CHAITALI PATEL.</span>
-            <br /> Web Developer
-            </h1>
-            <hr/>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar nisl quis ex cursus sollicitudin. Mauris eu elit non ligula rutrum venenatis. Praesent id sodales leo. Quisque aliquam blandit eros a gravida.  Sed venenatis, leo id tincidunt molestie, risus lorem ornare lorem, nec pellentesque est eros vitae turpis. Praesent nisl nulla, maximus quis est et, hendrerit semper nisl.
-            </p>
+        <section className="about container">
+            <div>
+                <img src={profileImage} alt="chaitali patel photo" />
+            </div>
+            <div className="aboutContent">
+                <h1 id="about"> Hi, 
+                <br /> <span className="highlightName">I'M CHAITALI PATEL.</span>
+                <br /> Web Developer
+                </h1>
+                <hr/>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar nisl quis ex cursus sollicitudin. Mauris eu elit non ligula rutrum venenatis. Praesent id sodales leo. Quisque aliquam blandit eros a gravida.  Sed venenatis, leo id tincidunt molestie, risus lorem ornare lorem, nec pellentesque est eros vitae turpis. Praesent nisl nulla, maximus quis est et, hendrerit semper nisl.
+                </p>
+            </div>
         </section>
     )
 };
